685c524d1adc9a31f2e7f3467ab0f6f3647217bc
peny753 <60744158+peny753@users.noreply.github.com>
2024-02-21 14:14:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import ogm_rare as rare
 
 
 if __name__ == '__main__':
-	# chip.process_data('table_folder/4/BRNO1440_3261.csv', 'table_folder/4/Dedup_1000distDistanced_BRNO1440.tsv', 'table_folder/4/result.tsv', 0, 10000)
+	chip.process_data('table_folder/4/BRNO1440_3261.csv', 'table_folder/4/Dedup_1000distDistanced_BRNO1440.tsv', 'table_folder/4/result.tsv', 0, 10000)
 	# assembly.process_data('table_folder/1/OGM Assambly.csv', 'table_folder/1/Dedup_1000distDistanced_BRNO2641_copy.tsv', 'table_folder/1/result_assembly.tsv', 0, 10000)
-	rare.process_data('table_folder/1/OGM Rare Analysis.csv', 'table_folder/1/Dedup_1000distDistanced_BRNO2641_copy.tsv', 'table_folder/1/result_rare.tsv', 10000, 20000)
+	# rare.process_data('table_folder/1/OGM Rare Analysis.csv', 'table_folder/1/Dedup_1000distDistanced_BRNO2641_copy.tsv', 'table_folder/1/result_rare.tsv', 10000, 20000)
 
