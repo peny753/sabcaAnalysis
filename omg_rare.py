6b9a38a60d0f6928b8839f9ff7b3075b48bb788b
peny753 <60744158+peny753@users.noreply.github.com>
2024-02-21 13:41:08 +0100

    Update omg_rare.py

--- omg_rare.py
+++ omg_rare.py
@@ -1,1 +1,13 @@
-#--
+import pandas as pd
+import csv
+
+
+
+def load_data_reference(filename):
+	reference = pd.read_csv(filename, delimiter=';')
+	return reference
+
+
+def load_table(filename):
+	data = pd.read_csv(filename, delimiter='\t', low_memory=False, index_col=0)
+	return data
